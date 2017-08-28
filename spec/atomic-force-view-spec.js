c3a5614fb3a273db5fa5a9d97afdd056a46faabe
Alex <alexrichey1@gmail.com>
2017-08-28 01:37:15 -0400

    Initial Commit!

---
+++ spec/atomic-force-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomicForceView from '../lib/atomic-force-view';
+
+describe('AtomicForceView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
